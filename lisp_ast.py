a32947ea2a00c1f36fbebeebe34daa55bc5e50d8
Peter Kang <peterjkang@gmail.com>
2024-02-12 11:06:05 -0500

    Post-interview

--- lisp_ast.py
+++ lisp_ast.py
@@ -45,11 +45,45 @@ def pprint_result(src):
     print(src, '\n  ->', lisp_ast(src))
 
 
+def evaluate_statement(ast):
+    op = ast[0]
+    args = ast[1:]
+
+    casted_args = []
+    for arg in args:
+        if type(arg) == list:
+            casted_args.append(evaluate_statement(arg))
+        else:
+            casted_args.append(arg)
+
+    return evaluate_op(op, casted_args)
+
+
+def evaluate_op(op, args):
+    if op == "+":
+        return sum(args)
+    if op == "list":
+        return list(args)
+    if op == "first":
+        assert len(args) == 1
+        return args[0][0]
+    if op == "logand":
+        assert len(args) == 2
+        return args[0] & args[1]
+
+
 if __name__ == "__main__":
     src = "(first (list 1 (+ 2 3) 9))"
     assert lisp_ast(src) == ["first", ["list", 1, ["+", 2, 3], 9]]
     assert lisp_ast("(+ 2 3)") == ["+", 2, 3]
     assert lisp_ast("(+ a b)") == ["+", 'a', 'b']
 
-    pprint_result(src)
-    pprint_result("(+ 2 3)")
+    # pprint_result(src)
+    # pprint_result("(+ 2 3)")
+
+    print(evaluate_statement(["+", 2, 3]))
+    print(evaluate_statement(["+", 1, ["+", 2, 3], 4]))  # 1 + (2 + 3) + 4
+    print(evaluate_statement(["list", 1, ["+", 2, 3], 9]))  # 1, 5, 9
+    print(evaluate_statement(["first", ["list", 1, ["+", 2, 3], 9]]))  # 1
+    print(evaluate_statement(["logand", 2, 3]))  # 10 & 11 -> 10
+    print(evaluate_statement(["+", 1, ["logand", 2, 3], 4]))  # 1 + 2 + 4 -> 7
